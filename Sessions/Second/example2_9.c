05aeba75e2efcd2bdaeb520425fa3cfa00476e65
Oromion <caznaranl@uni.pe>
2018-06-15 01:25:43 -0500

    Update

--- Sessions/Second/example2_9.c
+++ Sessions/Second/example2_9.c
@@ -8,7 +8,7 @@ main()
 	printf("Enter su salario deseado mensual:");	/* 1 */
 	printf("  $_______\b\b\b\b\b\b\b");				/* 2 */
 	scanf("%f", &salary);
-	printf("\n\t$%.2f al mes is %.2f al año.", salary, salary*12.0);
+	printf("\n\t$%.2f al mes is %.2f al aï¿½o.", salary, salary*12.0);
 	printf("\rGee!\n");
 
 }
